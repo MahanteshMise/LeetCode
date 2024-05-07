a3ac9dc6d58606fa1f24cdb5a6d12bb3a4581c95
Mahantesh <mahanteshmise13@gmail.com>
2024-05-07 23:27:58 +0530

    Two classes in separate file First and Second

---
+++ OOPS/First11.java
@@ -0,0 +1,8 @@
+public class First11 {
+    public void fullThrottle(){
+        System.out.println("The car is going as fast as it can!");
+    }
+    public void speed(int maxSpeed){
+        System.out.println("The max speed of car"+maxSpeed);
+    }
+}
---
+++ OOPS/Second11.java
@@ -0,0 +1,7 @@
+public class Second11 {
+    public static void main(String[] args) {
+        First11 myCar = new First11();
+        myCar.fullThrottle();
+        myCar.speed(100);
+    }
+}
