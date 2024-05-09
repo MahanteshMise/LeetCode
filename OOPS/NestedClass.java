1160d1baf25815869e23ad5756c8ba56c6a9e5a4
Mahantesh <mahanteshmise13@gmail.com>
2024-05-09 18:20:14 +0530

    Adding static nested if statement

--- OOPS/NestedClass.java
+++ OOPS/NestedClass.java
@@ -1,15 +1,22 @@
 public class NestedClass {
     int x = 10;
-    public class InnerClass{
+    static class InnerClass{
         int y =20;
     }
 }
-class Main111 {
+/*class Main111 {
     public static void main(String[] args) {
         NestedClass obj = new NestedClass();
         System.out.println(obj.x);
         // The way below is declared is very important
         NestedClass.InnerClass nestedObj = obj.new InnerClass();
         System.out.println(nestedObj.y);
+    }
+}*/
+//Accessing object using static method in Java
+class Main111{
+    public static void main(String[] args){
+        NestedClass.InnerClass obj = new NestedClass.InnerClass();
+        System.out.println(obj.y);
     }
 }
